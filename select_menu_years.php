78a14497dc98336f9d8c03c9c8b5d6f2a6c37f6c
keysman59 <keysman.dima@yandex.ru>
2018-08-02 03:40:03 +0300

    first

---
+++ select_menu_years.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>        </title>
+</head>
+<body>
+	<select>
+	<?php
+		echo "string";
+		for ($i = 1910; $i <= 2010 ; $i++) { 
+			echo "<option value=\"$i\">$i</option>";
+		}
+	?>
+	</select>
+</body>
+</html>
